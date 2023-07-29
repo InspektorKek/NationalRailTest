8c7ebe00adb71b3057c6d50a0975882e2d7e133e
Mikhail Borisov <borisovdiag@gmail.com>
2023-07-29 12:14:20 +0200

    Initial Commit

---
+++ NationalRailTest/NationalRailTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NationalRailTestApp.swift
+//  NationalRailTest
+//
+//  Created by Mikhail Borisov on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NationalRailTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
